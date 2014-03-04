4eef413e07bab078fcbc16516f2a8e60ec5af4da
Diogo Teixeira <diogo.andre.teixeira@gmail.com>
2014-03-04 21:32:11 +0000

    Applications mounted on new endpoints (solved reverse proxy problems).

--- config/apps.rb
+++ config/apps.rb
@@ -34,5 +34,5 @@ end
 
 # Mounts the core application for this project
 
-Padrino.mount("PbsSite::Admin", :app_file => Padrino.root('admin/app.rb')).to("/admin")
-Padrino.mount('PbsSite::App', :app_file => Padrino.root('app/app.rb')).to('/')
+Padrino.mount("PbsSite::Admin", :app_file => Padrino.root('admin/app.rb')).to("/pbsfinder/admin")
+Padrino.mount('PbsSite::App', :app_file => Padrino.root('app/app.rb')).to('/pbsfinder')
